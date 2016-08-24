bc52697420c4551232596a485b7a07b0ac7ffa35
Valuks <termilv@gmail.com>
2016-08-24 11:10:14 +0300

    removed copy&paste foreign fb:app_id

--- public/d/j.js
+++ public/d/j.js
@@ -49,6 +49,6 @@ var js, fjs = d.getElementsByTagName(s)[0];
 if (d.getElementById(id)) return;
 js = d.createElement(s);
 js.id = id;
-js.src = "//connect.facebook.net/lv_LV/sdk.js#xfbml=1&version=v2.7&appId=130244563813048";
+js.src = "//connect.facebook.net/lv_LV/sdk.js#xfbml=1&version=v2.7";
 fjs.parentNode.insertBefore(js, fjs);
 })(document, "script", "facebook-jssdk");
